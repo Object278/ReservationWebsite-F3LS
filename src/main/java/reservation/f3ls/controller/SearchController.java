b111eb131ea16acfbfa5c4d6a6ee8267aa9fe3af
Object278 <zli18876@gmail.com>
2022-06-15 21:08:01 +0200

    Filter implementation

--- src/main/java/reservation/f3ls/controller/SearchController.java
+++ src/main/java/reservation/f3ls/controller/SearchController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class SearchController {
     private final SearchService service;
 
+
     @Autowired
     public SearchController(SearchService service){
         this.service = service;
